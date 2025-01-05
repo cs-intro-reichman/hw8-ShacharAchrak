a8f452e818c9105675f582d6783b85eba3062176
ShacharAchrak <shachar.achrak@post.runi.ac.il>
2025-01-05 17:10:37 +0200

    Network

--- Network.java
+++ Network.java
@@ -31,7 +31,7 @@ public class Network {
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
         for (int i = 0; i < this.userCount; i++){
-            if(users[i].getName().equals(name)){
+            if(users[i].getName().equalsIgnoreCase(name)){
                 return users[i];
             }
         }
